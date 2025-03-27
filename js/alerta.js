14aac4e150d4e9e3dd555643bc5fd94a07bcd264
Chaylane Franco <136913090+Chayfranco@users.noreply.github.com>
2025-03-27 17:16:09 -0300

    ok

--- js/alerta.js
+++ js/alerta.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getDatabase, ref, onValue, push, set } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyA6lwFb8AaEE8T0l5jn9hEtvUWsAoPp9A",
+    apiKey: "AIzaSyA6lwFb8AaEE8T0l5jn9hEtvUWsAoPp9pA",
     authDomain: "elevsaf.firebaseapp.com",
     databaseURL: "https://elevsaf-default-rtdb.firebaseio.com",
     projectId: "elevsaf",
@@ -15,18 +16,17 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-// ID permitido
-const allowedUserId = "wqcbJ139uRXyk8wts5CAzP489W32";
+const auth = getAuth();
+
 // Elementos do DOM
 const distanciaSensorInput = document.getElementById("distanciaSensor");
 const distanciaAguaInput = document.getElementById("distanciaAgua");
 const alertMessage = document.getElementById("alertMessage");
 const tabelaHistorico = document.getElementById("tabela-historico");
-
+const allowedUserId = "wqcbJ139uRXyk8wts5CAzP489W32";
 // Função para verificar se o usuário está logado e tem o ID correto
 onAuthStateChanged(auth, (user) => {
     if (user) {
-        // Verificar se o UID do usuário corresponde ao permitido
         if (user.uid === allowedUserId) {
             // Atualizar os valores em tempo real
             onValue(ref(database, "sensor/distanciaReal"), (snapshot) => {
